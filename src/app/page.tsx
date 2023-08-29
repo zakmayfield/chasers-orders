4abe1cf9466bdad6a07957715b1618d24e466d73
Zak Mayfield <zakmayfield@gmail.com>
2023-08-29 14:28:59 -0400

    chore: change headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1>Chasers Fresh Juice</h1>
+      <h2>Orders</h2>
     </main>
   );
 }
