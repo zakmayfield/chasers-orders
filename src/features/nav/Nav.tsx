cf973d20e402663074c52ca80d5132299383ba1b
Zak Mayfield <zakmayfield@gmail.com>
2023-08-29 14:30:02 -0400

    chore: logging & link to products

--- src/features/nav/Nav.tsx
+++ src/features/nav/Nav.tsx
@@ -4,9 +4,11 @@ import { getAuthSession } from '@/lib/auth';
 
 export default async function Nav() {
   const session = await getAuthSession();
+  console.log('----- session -----', session);
   return (
     <nav className='flex items-center gap-6'>
       <Link href='/'>Home</Link>
+      <Link href='/products'>Shop</Link>
 
       {session?.user ? <UserNav /> : <Link href='/sign-in'>Sign In</Link>}
     </nav>
