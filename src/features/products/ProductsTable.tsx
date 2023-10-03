3ea295844086a793d27e4647e2159f6267112aa4
Zak Mayfield <zakmayfield@gmail.com>
2023-10-03 13:43:13 -0400

    chore: products table body

--- src/features/products/ProductsTable.tsx
+++ src/features/products/ProductsTable.tsx
@@ -15,6 +15,7 @@ export default function ProductsTable({
 }: {
   products: Product[];
 }) {
+  console.log(data);
   const columnHelper = createColumnHelper<TableProduct>();
 
   const columns = [
@@ -38,7 +39,37 @@ export default function ProductsTable({
 
   return (
     <div>
-      <h1>products</h1>
+      <h1>products table</h1>
+
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
