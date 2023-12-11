45df99de53e3c1b5891db73d9ea52413b3e86f25
Zak Mayfield <zakmayfield@gmail.com>
2023-12-11 10:42:22 -0500

    feat: conditionally render certain links from auth

--- src/features/nav/UserNav.tsx
+++ src/features/nav/UserNav.tsx
@@ -1,6 +1,13 @@
 'use client';
 import { signOut } from 'next-auth/react';
+import Link from 'next/link';
 
 export default function UserNav() {
-  return <button onClick={() => signOut()}>Logout</button>;
+  return (
+    <div className='flex items-center gap-6'>
+      <Link href='/cart'>Cart</Link>
+      <Link href='/profile'>Profile</Link>
+      <button onClick={() => signOut()}>Logout</button>
+    </div>
+  );
 }
