42b83cfcf502ff3bfa06227f00084837f3264765
Zak Mayfield <zakmayfield@gmail.com>
2024-01-25 11:56:40 -0500

    chore: adjust next auth types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,21 +1,24 @@
 import { Session, User } from 'next-auth';
 import { JWT } from 'next-auth/jwt';
-type UserId = string;
-
-declare module 'next-auth/jwt' {
-  interface JWT {
-    id: UserId;
-    username?: string | null;
-    isApproved: boolean;
-  }
-}
 
 declare module 'next-auth' {
   interface Session {
     user: User & {
-      id: UserId;
-      username?: string | null;
+      id: string;
       isApproved: boolean;
     };
+  }
+}
+
+declare module 'next-auth' {
+  interface User {
+    isApproved: boolean;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string;
+    isApproved: boolean;
   }
 }
