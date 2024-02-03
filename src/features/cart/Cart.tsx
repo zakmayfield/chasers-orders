38e3ad115d45139bbae6c24769738ec5adb7a6d4
Zak Mayfield <zakmayfield@gmail.com>
2024-02-03 14:23:16 -0500

    feat: cart: conditional mounting

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -4,9 +4,9 @@ import { useQuery } from '@tanstack/react-query';
 import { getCart } from '@/store/cart.get';
 import { CartCache } from '@/types/types.cart';
 import { ToastContainer } from 'react-toastify';
-import CartItem from './ui/CartItem';
 import EmptyCart from './ui/EmptyCart';
 import Link from 'next/link';
+import CartItemContainer from './ui/CartItemContainer';
 
 export default function Cart() {
   const { isLoading, isError, data, error } = useQuery<
@@ -28,24 +28,9 @@ export default function Cart() {
 
   return (
     <div>
-      <div>
-        {data && data.items.length < 1 && <EmptyCart />}
-
-        {data &&
-          data.items.length > 0 &&
-          data.items.map((item) => (
-            <CartItem
-              key={item.unitId}
-              payload={{
-                cartId: data.id,
-                cartItem: item,
-              }}
-            />
-          ))}
-      </div>
-
+      {data && data.items.length < 1 && <EmptyCart />}
+      {data && data.items.length > 0 && <CartItemContainer cartData={data} />}
       <Link href='/cart/order'>Confirm Order</Link>
-
       <ToastContainer />
     </div>
   );
