1397a5861a29b444736217d758d2f019e92e95ea
Zak Mayfield <zakmayfield@gmail.com>
2024-02-03 19:11:39 -0500

    feat: trigger.dev integration send order email

---
+++ src/trigger.ts
@@ -0,0 +1,7 @@
+import { TriggerClient } from "@trigger.dev/sdk";
+
+export const client = new TriggerClient({
+  id: "chasers-juice-jobs-r5f1",
+  apiKey: process.env.TRIGGER_API_KEY,
+  apiUrl: process.env.TRIGGER_API_URL,
+});
