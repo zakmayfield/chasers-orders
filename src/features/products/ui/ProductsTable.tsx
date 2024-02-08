a9fa94c0f031479e5e9343579295a8e0a8c28891
Zak Mayfield <zakmayfield@gmail.com>
2024-02-08 13:18:51 -0500

    chore: remove extended flag

--- src/features/products/ui/ProductsTable.tsx
+++ src/features/products/ui/ProductsTable.tsx
@@ -39,7 +39,7 @@ export default function ProductsTable({
   products: ProductWithUnits[];
 }) {
   const queryClient = useQueryClient();
-  const { favorites } = useFavoritesQuery({ extended: true });
+  const { favorites } = useFavoritesQuery();
   const { notify, ToastContainer } = useToast();
 
   const [selectedUnits, setSelectedUnits] = useState<Array<Unit | null>>(
