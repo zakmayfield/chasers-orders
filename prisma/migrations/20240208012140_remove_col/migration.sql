300d1eb1444b98b50afd9eff5a3a0e699d8a25af
Zak Mayfield <zakmayfield@gmail.com>
2024-02-07 20:35:55 -0500

    feat: db table - favorite

---
+++ prisma/migrations/20240208012140_remove_col/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `size` on the `Favorite` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Favorite" DROP COLUMN "size";
