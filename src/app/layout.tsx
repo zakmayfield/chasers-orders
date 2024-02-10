276d823fb5aa9d5819f2ee79527e6614522d8001
Zak Mayfield <zakmayfield@gmail.com>
2024-02-10 10:30:04 -0500

    chore: lib providers definiton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Nav from '../features/nav/Nav';
-import Providers from '@/lib/providers/Providers';
+import Providers from '@/lib/Providers';
 import { ToastContainer } from 'react-toastify';
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
