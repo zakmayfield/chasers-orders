6fd527a0e376df61e6a01f1276c1a6c663b794b8
Zak Mayfield <zakmayfield@gmail.com>
2024-02-10 10:26:38 -0500

    chore: utils

--- src/lib/auth/auth.config.ts
+++ src/lib/auth/auth.config.ts
@@ -11,10 +11,10 @@ import {
   extractExpiration,
   generateVerificationToken,
 } from '@/utils/token.utils';
-import { sendVerificationEmail } from '@/utils/email/email.send-verification-email';
+import { sendVerificationEmail } from '@/utils/email.utils';
 import { db } from '@/lib/db/db.prisma-client';
-import { findUniqueSecureUser, registerUser } from '@/lib/db/db.user';
-import { createCart } from '@/lib/db/db.cart';
+import { findUniqueSecureUser, registerUser } from '@/utils/auth.utils';
+import { createCart } from '@/utils/cart.utils';
 
 // adapter
 type NextAuthAdapter = NextAuthOptions['adapter'];
--- src/utils/auth.utils.ts
+++ src/utils/auth.utils.ts
@@ -1,3 +1,6 @@
+import { db } from '@/lib/db/db.prisma-client';
+import { SignUpFormData } from '@/types/types.auth-forms';
+import { User } from '@prisma/client';
 import { JWT } from 'next-auth/jwt';
 
 /*
@@ -28,4 +31,97 @@ export const userStatus: ResolvedVerificationCheck = async (token) => {
   }
 
   return { isApproved: token!.isApproved, emailVerified: token!.emailVerified };
+};
+
+/*
+  GET SECURE USER
+*/
+
+type UniqueSecureUser = {
+  (email: string): Promise<SecureUser | null>;
+};
+
+type SecureUser = Omit<User, 'password' | 'image'>;
+
+export const findUniqueSecureUser: UniqueSecureUser = async (email) =>
+  await db.user.findUnique({
+    where: { email },
+    select: {
+      id: true,
+      email: true,
+      isApproved: true,
+      emailVerified: true,
+    },
+  });
+
+/*
+  REGISTER USER
+*/
+
+interface IRegisterUser {
+  (payload: RegisterUserPayload): Promise<SecureUser | null>;
+}
+
+type RegisterUserPayload = {
+  credentials: SignUpFormData;
+  hashedPassword: string;
+  verificationToken: string;
+  expires: Date;
+};
+
+export const registerUser: IRegisterUser = async (payload) => {
+  const { credentials, hashedPassword, verificationToken, expires } = payload;
+
+  return await db.user.create({
+    data: {
+      email: credentials.email,
+      password: hashedPassword,
+      verificationToken: {
+        create: {
+          identifier: `email-verification-${credentials.email}`,
+          token: verificationToken,
+          expires,
+        },
+      },
+      contact: {
+        create: {
+          name: credentials.contactName,
+          position: credentials.contactPosition,
+          phoneNumber: credentials.contactPhoneNumber,
+        },
+      },
+      company: {
+        create: {
+          name: credentials.companyName,
+          accountPayableEmail: credentials.accountPayableEmail,
+          paymentMethod: credentials.paymentMethod,
+          shippingAddress: {
+            create: {
+              streetAddress: credentials.shippingStreetAddress,
+              unit: credentials.shippingUnit,
+              city: credentials.shippingCity,
+              state: credentials.shippingState,
+              postalCode: credentials.shippingPostalCode,
+              deliveryInstructions: credentials.deliveryInstructions,
+            },
+          },
+          billingAddress: {
+            create: {
+              streetAddress: credentials.billingStreetAddress,
+              unit: credentials.billingUnit,
+              city: credentials.billingCity,
+              state: credentials.billingState,
+              postalCode: credentials.billingPostalCode,
+            },
+          },
+        },
+      },
+    },
+    select: {
+      id: true,
+      email: true,
+      isApproved: true,
+      emailVerified: true,
+    },
+  });
 };
