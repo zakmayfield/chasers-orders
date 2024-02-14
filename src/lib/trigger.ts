e8d5ee3a62976a76f00dfcc19555f070e837d065
Zak Mayfield <zakmayfield@gmail.com>
2024-02-14 07:55:43 -0500

    chore: todo

--- src/lib/trigger.ts
+++ src/lib/trigger.ts
@@ -1,7 +1,11 @@
-import { TriggerClient } from "@trigger.dev/sdk";
+import { TriggerClient } from '@trigger.dev/sdk';
+
+/*
+  TODO: Integrate trigger into various jobs
+*/
 
 export const client = new TriggerClient({
-  id: "chasers-juice-jobs-r5f1",
+  id: 'chasers-juice-jobs-r5f1',
   apiKey: process.env.TRIGGER_API_KEY,
   apiUrl: process.env.TRIGGER_API_URL,
 });
