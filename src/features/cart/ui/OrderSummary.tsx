ceabc8f80b6c454ec910e3fb73536ac262384128
Zak Mayfield <zakmayfield@gmail.com>
2024-02-16 18:11:54 -0500

    feat: shipping information section on order summary

--- src/features/cart/ui/OrderSummary.tsx
+++ src/features/cart/ui/OrderSummary.tsx
@@ -3,12 +3,17 @@
 import GridContainer from '@/features/ui/layout/GridContainer';
 import SummaryDetails from './SummaryDetail';
 import OrderButton from './OrderButton';
+import ShippingDetails from './ShippingDetails';
 
 const OrderSummary = () => {
   return (
     <div className='col-start-8 col-span-3'>
       <GridContainer cols={3}>
+        {/* Order Summary */}
         <SummaryDetails />
+
+        {/* Shipping Details */}
+        <ShippingDetails />
 
         {/* Order button */}
         <OrderButton />
---
+++ src/services/queries/cart.getShipping.ts
@@ -0,0 +1,20 @@
+import { GetShippingPayload } from '@/app/api/user/company/shipping/route';
+
+export const getShipping = async (): Promise<
+  GetShippingPayload | undefined
+> => {
+  try {
+    const response = await fetch('/api/user/company/shipping');
+
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+};
