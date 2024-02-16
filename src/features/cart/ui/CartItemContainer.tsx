fe1dca3a5875ebf7ef3be6cd3ff14f9911b725ff
Zak Mayfield <zakmayfield@gmail.com>
2024-02-16 18:40:48 -0500

    feat: fetching state for cart header quantity number

--- src/features/cart/ui/CartItemContainer.tsx
+++ src/features/cart/ui/CartItemContainer.tsx
@@ -4,9 +4,10 @@ import { CartCache } from '@/types/types.cart';
 import CartItem from './CartItem';
 import { useQuery } from '@tanstack/react-query';
 import { getCart } from '@/services/queries/cart.getCart';
+import { ImSpinner2 } from 'react-icons/im';
 
 export default function CartItemContainer() {
-  const { data } = useQuery<CartCache | undefined, Error>({
+  const { data, isFetching } = useQuery<CartCache | undefined, Error>({
     queryKey: ['cart'],
     queryFn: getCart,
     staleTime: Infinity,
@@ -14,6 +15,21 @@ export default function CartItemContainer() {
 
   return (
     <div className='col-start-3 col-span-4'>
+      {/* Cart Items Container Header */}
+      <div className='flex items-center gap-3 font-extralight'>
+        <h4 className='h-24 text-2xl flex items-center pl-12'>Cart</h4>
+        {isFetching ? (
+          <span className='animate-spin'>
+            <ImSpinner2 />
+          </span>
+        ) : (
+          <p>
+            (<span>{data?.items.length}</span>)
+          </p>
+        )}
+      </div>
+
+      {/* Itemz */}
       {data &&
         data.items.map((item) => (
           <CartItem
