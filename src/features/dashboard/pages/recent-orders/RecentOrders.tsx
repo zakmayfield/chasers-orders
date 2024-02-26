46e8d356b0accee786bd08cd0ebec7f27b296bfc
Zak Mayfield <zakmayfield@gmail.com>
2024-02-26 09:56:22 -0500

    feat: recent order loading skelly

--- src/features/dashboard/pages/recent-orders/RecentOrders.tsx
+++ src/features/dashboard/pages/recent-orders/RecentOrders.tsx
@@ -1,19 +1,56 @@
 'use client';
 
-import { getRecentOrders } from '@/services/queries/orders.getRecentOrders';
+import { useState } from 'react';
 import { Order, OrderLineItem } from '@prisma/client';
-import { useQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { ThreeDots } from 'react-loader-spinner';
+import { PiSpinnerGapThin } from 'react-icons/pi';
+import { getRecentOrders } from '@/services/queries/orders.getRecentOrders';
 
 export type OrderType = Order & {
   lineItems: OrderLineItem[];
 };
 
 const RecentOrders = () => {
-  const params = useSearchParams();
+  const {
+    data: orders,
+    error,
+    isLoading,
+  } = useQuery<OrderType[]>({
+    queryKey: ['recent-orders'],
+    queryFn: getRecentOrders,
+    staleTime: Infinity,
+  });
+
+  return (
+    <div>
+      <RecentOrdersHeader isLoading={isLoading} />
+      <RecentOrdersContent />
+    </div>
+  );
+};
+
+export default RecentOrders;
+
+function RecentOrdersHeader({ isLoading }: { isLoading: boolean }) {
+  function Spinner() {
+    return (
+      <span className='ml-3'>
+        <PiSpinnerGapThin className='text-xl animate-spin' />
+      </span>
+    );
+  }
+
+  return (
+    <h2 className='flex items-center mb-6'>
+      Recent Orders {isLoading && <Spinner />}
+    </h2>
+  );
+}
+
+function RecentOrdersContent() {
   const [expanded, setExpanded] = useState<OrderType | null>();
-  const orderId = params.get('orderId');
 
   const {
     data: orders,
@@ -22,20 +59,35 @@ const RecentOrders = () => {
   } = useQuery<OrderType[]>({
     queryKey: ['recent-orders'],
     queryFn: getRecentOrders,
-    /*
-      `recent-orders` cache will only get fetched on initial page load &
-      cache data will update within the onSuccess of the createOrder mutation
-    */
     staleTime: Infinity,
   });
 
-  useEffect(() => {
-    const orderFromParams = orders?.find((order) => order.id === orderId);
-    setExpanded(orderFromParams);
-  }, [orderId, orders]);
+  if (isLoading) {
+    return <RecentOrdersSkeleton />;
+  }
+  function RecentOrdersSkeleton() {
+    return (
+      <div className='w-4/5 flex flex-col gap-6'>
+        {[1, 2, 3, 4, 5].map((item) => (
+          <div
+            key={item}
+            className={`
+              h-10 rounded-lg animate-pulse 
+              even:bg-light-secondary/80 
+              odd:bg-light-tertiary/50 odd:mr-28 
+            `}
+          ></div>
+        ))}
+      </div>
+    );
+  }
 
   if (error && error instanceof Error) {
     return <div>{error.message}</div>;
+  }
+
+  if (orders && orders.length === 0) {
+    return <p>No recent orders</p>;
   }
 
   const handleExpanded = (order: OrderType) => {
@@ -48,11 +100,6 @@ const RecentOrders = () => {
 
   return (
     <div>
-      <div>Recent Orders</div>
-      {orders && orders.length === 0 && <p>No recent orders.</p>}
-
-      {isLoading && <div>Loading recent orders</div>}
-
       {orders &&
         orders.map((order) => {
           const createdAtDate = new Date(order.createdAt).toDateString();
@@ -68,6 +115,4 @@ const RecentOrders = () => {
         })}
     </div>
   );
-};
-
-export default RecentOrders;
+}
