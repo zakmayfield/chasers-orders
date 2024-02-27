9c3d442ecae93849766cbd5d2cb213b55b183cf9
Zak Mayfield <zakmayfield@gmail.com>
2024-02-27 17:59:09 -0500

    feat: favorites loading spinner

--- src/features/dashboard/pages/favorites/Favorites.tsx
+++ src/features/dashboard/pages/favorites/Favorites.tsx
@@ -4,13 +4,10 @@ import { useFavoritesQuery } from '@/hooks/queries/useFavoritesQuery';
 import Favorite from './components/Favorite';
 import FavoritesLoadingSkeleton from './components/FavoritesLoadingSkeleton';
 import EmptyFavorites from './components/EmptyFavorites';
+import LoadingSpinner from '@/features/spinner/LoadingSpinner';
 
 export default function Favorites() {
   const { favorites, isLoading } = useFavoritesQuery();
-
-  if (isLoading) {
-    return <FavoritesLoadingSkeleton />;
-  }
 
   if (favorites && favorites.length === 0) {
     return <EmptyFavorites />;
@@ -18,10 +15,19 @@ export default function Favorites() {
 
   return (
     <div>
-      <div>Favorites</div>
-      {favorites &&
-        favorites.length > 0 &&
-        favorites.map((fav) => <Favorite key={fav.id} fav={fav} />)}
+      <h2 className='flex items-center mb-6'>
+        Favorites {isLoading && <LoadingSpinner />}
+      </h2>
+
+      {isLoading ? (
+        <FavoritesLoadingSkeleton />
+      ) : (
+        <div>
+          {favorites &&
+            favorites.length > 0 &&
+            favorites.map((fav) => <Favorite key={fav.id} fav={fav} />)}
+        </div>
+      )}
     </div>
   );
 }
--- src/features/dashboard/pages/favorites/components/FavoritesLoadingSkeleton.tsx
+++ src/features/dashboard/pages/favorites/components/FavoritesLoadingSkeleton.tsx
@@ -4,11 +4,11 @@ interface FavoritesLoadingSkeletonProps {}
 
 const FavoritesLoadingSkeleton: FC<FavoritesLoadingSkeletonProps> = ({}) => {
   return (
-    <div className='w-full flex flex-col gap-6'>
+    <div className='w-full flex flex-col'>
       {[1, 2, 3].map((item) => (
-        <div className='w-full flex items-center gap-3'>
+        <div key={item} className='w-full h-16 flex items-center gap-3'>
           <div className='bg-light-secondary/70 animate-pulse w-12 h-12 rounded-lg'></div>
-          <div className='bg-light-secondary/70 animate-pulse w-72 h-12 rounded-lg'></div>
+          <div className='bg-light-tertiary/30 animate-pulse w-72 h-12 rounded-lg'></div>
           <div className='bg-light-secondary/70 animate-pulse w-36 h-12 rounded-lg'></div>
         </div>
       ))}
