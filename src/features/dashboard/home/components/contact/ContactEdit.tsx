17bff6b4896929bf903d1a6a70eb8e0ce985498f
Zak Mayfield <zakmayfield@gmail.com>
2024-02-29 12:35:35 -0500

    chore: cleanup

--- src/features/dashboard/home/components/contact/ContactEdit.tsx
+++ src/features/dashboard/home/components/contact/ContactEdit.tsx
@@ -47,24 +47,3 @@ export const ContactEdit: FC<ContactEditProps> = ({ userData }) => {
     </form>
   );
 };
-
-/*
-    <div className='flex flex-col'>
-      <div className='grid grid-cols-10 gap-3'>
-        <span className='col-span-3 text-gray-700'>Name: </span>
-        <span className='col-start-5 col-span-6'>{userData.contact.name}</span>
-
-        <span className='row-start-2 col-span-4 text-gray-700'>
-          Phone number:{' '}
-        </span>
-        <span className='row-start-2 col-start-5 col-span-6'>
-          {userData.contact.phoneNumber}
-        </span>
-
-        <span className='row-start-3 col-span-4 text-gray-700'>Position: </span>
-        <span className='row-start-3 col-start-5 col-span-6'>
-          {userData.contact.position && userData.contact.position}
-        </span>
-      </div>
-    </div>
-*/
