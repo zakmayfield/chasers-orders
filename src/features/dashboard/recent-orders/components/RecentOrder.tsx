2826a75879a550adde3abad3429b0e81fb589ed9
Zak Mayfield <zakmayfield@gmail.com>
2024-03-02 09:49:30 -0500

    feat: recent order items component

--- src/features/dashboard/recent-orders/components/RecentOrder.tsx
+++ src/features/dashboard/recent-orders/components/RecentOrder.tsx
@@ -1,18 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { PiSpinnerGapThin, PiWarningDuotone } from 'react-icons/pi';
 import {
   LineItemProducts,
   fetchLineItemsFromOrderId,
 } from '@/services/queries/orders.fetchLineItemsFromOrderId';
-import { useQuery } from '@tanstack/react-query';
 import { OrderType } from '../RecentOrders';
-import { PiSpinnerGapThin, PiWarningDuotone } from 'react-icons/pi';
 import OrderAgainButton from './OrderAgainButton';
+import { RecentOrderItems } from './RecentOrderItems';
 
 const RecentOrder = ({ order }: { order: OrderType }) => {
   const {
-    data: orderWithLineItemProducts,
+    data: orderWithLineItems,
     isLoading,
     isError,
-  } = useQuery<LineItemProducts | null>({
+  } = useQuery<LineItemProducts | undefined>({
     queryKey: ['line-item-products', order.id],
     queryFn: () => fetchLineItemsFromOrderId(order.id),
     staleTime: Infinity,
@@ -31,26 +32,6 @@ const RecentOrder = ({ order }: { order: OrderType }) => {
     );
   }
 
-  /** 
-    "order again":
-      goal: when clicked, add items from order to the cart and redirect user to the cart, 
-            or a tool tip sort of 'notice' that pops up with a link to the cart, in case the user isn't ready to check out?? not sure.
-      Flow:
-        - click order again
-        - add order items to the cart
-          - will need:
-            - handleAddToCart mutation
-            - ability to fetch the cart API to POST new cart records (from the recent order)
-        - on add to cart success:
-          - redirect user to /cart |OR| the tool tip thing
-
-      TODO:
-        - ✅ handleAddToCart mutation
-        - ✅ ability to fetch the cart API
-        - ⏳ set cart cache with new data from order items
-        - ⏳ redirect on success
-  */
-
   return (
     <div key={order.id} className=''>
       <div className='mb-2 w-full flex items-center gap-3 '>
@@ -62,32 +43,7 @@ const RecentOrder = ({ order }: { order: OrderType }) => {
         {isLoading ? (
           <PiSpinnerGapThin className='animate-spin' />
         ) : (
-          <div>
-            {orderWithLineItemProducts?.lineItems.map(
-              ({ id, quantity, unit: { product } }) => {
-                return (
-                  <div className='grid grid-cols-10 mb-6 w-full' key={id}>
-                    <span className='col-span-1 text-sm w-full text-center text-gray-600 mr-3 font-normal underline'>
-                      x{quantity}
-                    </span>
-
-                    <span className='col-start-2 col-span-full w-full'>
-                      {product.name}
-                    </span>
-
-                    <span className='row-start-2 col-start-2 w-full text-sm italic text-gray-600 lowercase'>
-                      {product.category}
-                    </span>
-
-                    <div
-                      aria-label='style div'
-                      className='h-1 w-2/3 mx-auto mt-3 rounded row-start-3 col-span-full bg-light-secondary'
-                    ></div>
-                  </div>
-                );
-              }
-            )}
-          </div>
+          <RecentOrderItems order={orderWithLineItems} />
         )}
       </div>
     </div>
---
+++ src/features/dashboard/recent-orders/components/RecentOrderItems.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import { LineItemProducts } from '@/services/queries/orders.fetchLineItemsFromOrderId';
+
+interface RecentOrderItemsProps {
+  order: LineItemProducts | undefined;
+}
+
+export const RecentOrderItems: FC<RecentOrderItemsProps> = ({ order }) => {
+  return (
+    <div>
+      {order &&
+        order.lineItems.map(({ id, quantity, unit: { product } }) => {
+          return (
+            <div className='grid grid-cols-10 mb-6 w-full' key={id}>
+              <span className='col-span-1 text-sm w-full text-center text-gray-600 mr-3 font-normal underline'>
+                x{quantity}
+              </span>
+
+              <span className='col-start-2 col-span-full w-full'>
+                {product.name}
+              </span>
+
+              <span className='row-start-2 col-start-2 w-full text-sm italic text-gray-600 lowercase'>
+                {product.category}
+              </span>
+
+              <div
+                aria-label='style div'
+                className='h-1 w-2/3 mx-auto mt-3 rounded row-start-3 col-span-full bg-light-secondary'
+              ></div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
