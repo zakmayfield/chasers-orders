5d378d253c2b7dd4b881451be063f29fd1c8c111
Zak Mayfield <zakmayfield@gmail.com>
2024-03-06 19:29:28 -0500

    feat: Table loading UI

--- src/features/products/components/table/TableLoadingSkelly.tsx
+++ src/features/products/components/table/TableLoadingSkelly.tsx
@@ -1,11 +1,16 @@
 export const TableLoadingSkeleton = () => {
   return (
-    <div className='mt-24 pt-1'>
+    <div className='pt-1 h-[546px]'>
+      <div className='h-[105px] flex items-center gap-6 animate-pulse'>
+        <div className='h-3/4 rounded-lg w-1/2 bg-slate-100'></div>
+        <div className='h-3/4 rounded-lg w-1/4 bg-slate-100'></div>
+        <div className='h-3/4 rounded-lg w-1/4 bg-slate-100'></div>
+      </div>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
         return (
           <div
             key={item}
-            className='h-[45px] w-3/4 mx-auto even:bg-gray-100 animate-pulse'
+            className='h-[45px] w-3/4 mx-auto odd:bg-slate-50 animate-pulse'
           ></div>
         );
       })}
