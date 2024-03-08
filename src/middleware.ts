1e68308436c5d69cad6679b2946892f3bf283bd4
Zak Mayfield <zakmayfield@gmail.com>
2024-03-08 13:19:43 -0500

    feat: allowing origins for cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,36 @@ import { userStatus } from './utils/auth.utils';
   TODO: Implement wrapped middleware: https://next-auth.js.org/configuration/nextjs#advanced-usage
 */
 
+const allowedOrigins = [
+  'https://chasers-orders-git-dev-mayfieldcreates.vercel.app/',
+  'https://chasers-orders.vercel.app/',
+];
+
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
+
 export async function middleware(req: NextRequest) {
   const token: JWT | null = await getToken({ req });
 
   if (!token) {
     return NextResponse.redirect(new URL('/', req.nextUrl));
+  }
+
+  // Check the origin from the request
+  const origin = req.headers.get('origin') ?? '';
+  const isAllowedOrigin = allowedOrigins.includes(origin);
+
+  // Handle preflighted requests
+  const isPreflight = req.method === 'OPTIONS';
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
   }
 
   // Fetch user account status from token
