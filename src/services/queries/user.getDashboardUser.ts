23405974529254f3df1214733a26f0dc81adf5f6
Zak Mayfield <zakmayfield@gmail.com>
2024-03-09 08:09:54 -0500

    trying one more env var

--- src/services/queries/user.getDashboardUser.ts
+++ src/services/queries/user.getDashboardUser.ts
@@ -8,7 +8,7 @@ type GetDashboardUserDataProps = {
 export const getDashboardUser: GetDashboardUserDataProps = async () => {
   try {
     const envURL =
-      process.env.NODE_ENV === 'development'
+      process.env.VERCEL_ENV === 'development'
         ? process.env.NEXT_PUBLIC_DEV_BASE_URL
         : process.env.NEXT_PUBLIC_BASE_URL;
 
