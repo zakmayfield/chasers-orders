bfa3eedfd1e800330f62e22512b1128f384d13f1
Zak Mayfield <zakmayfield@gmail.com>
2024-03-11 16:43:38 -0400

    feat: sign in form state and UI

---
+++ src/features/auth/signin/components/SignInButton.tsx
@@ -0,0 +1,29 @@
+import LoadingSpinner from '@/features/shared/LoadingSpinner';
+import { FC } from 'react';
+
+interface SignInButtonProps {
+  isSubmitted: boolean;
+  isValid: boolean;
+  isSubmitSuccessful: boolean;
+}
+
+export const SignInButton: FC<SignInButtonProps> = ({
+  isSubmitted,
+  isValid,
+  isSubmitSuccessful,
+}) => {
+  return (
+    <button
+      type='submit'
+      className={`border-2 rounded-lg p-2 col-span-6 focus:ring-4 focus:ring-blue-400 mt-6 bg-light-greenish/70 text-lg text-white font-medium h-12
+                  ${isSubmitted && isSubmitSuccessful && 'bg-light-greenish/50'}`}
+      disabled={isSubmitted && isSubmitSuccessful}
+    >
+      {isSubmitted && isSubmitSuccessful ? (
+        <LoadingSpinner className='mx-auto text-2xl' />
+      ) : (
+        'Sign In'
+      )}
+    </button>
+  );
+};
