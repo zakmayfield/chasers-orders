4d12e8ed35cb8a32a0db135cac78a37432ac729e
Zak Mayfield <zakmayfield@gmail.com>
2024-03-11 14:57:12 -0400

    feat: footer: footer logo button

--- src/features/footer/components/FooterLogo.tsx
+++ src/features/footer/components/FooterLogo.tsx
@@ -1,13 +1,22 @@
+'use client';
 import { FC } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import logosmall from '@/assets/logo-sm.png';
 
 interface FooterLogoProps {}
 
 const FooterLogo: FC<FooterLogoProps> = ({}) => {
+  const router = useRouter();
   return (
     <div className='self-center'>
-      <Image src={logosmall} alt='chasers juice logo' width={75} />
+      <button
+        className='p-2 rounded-lg hover:ring-2'
+        onClick={() => router.push('/products')}
+        role='link'
+      >
+        <Image src={logosmall} alt='chasers juice logo' width={75} />
+      </button>
     </div>
   );
 };
