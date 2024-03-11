42dd96f7589b8f17916d4013b14f151d40deb3a6
Zak Mayfield <zakmayfield@gmail.com>
2024-03-11 13:44:33 -0400

    feat: not found component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import notfound from '@/assets/notfound.png';
+import Image from 'next/image';
+
+export default function NotFound() {
+  return (
+    <div className='flex flex-col items-center 2xl:flex-row 2xl:justify-center 2xl:items-end'>
+      <NotFoundImage />
+      <NotFoundInfo />
+    </div>
+  );
+}
+
+function NotFoundImage() {
+  return (
+    <div className=''>
+      <Image
+        src={notfound}
+        alt='illustration of a person riding a rocket away from the not found text'
+        width={400}
+        aria-describedby='png image from pngtree.com'
+        priority
+      />
+    </div>
+  );
+}
+
+function NotFoundInfo() {
+  return (
+    <div className='flex items-end'>
+      <div className='2xl:py-12'>
+        <h1>Not Found</h1>
+        <p className='mb-6 text-lg'>
+          We couldn&apos;t find that requested resource
+        </p>
+        <Link
+          href='/dashboard'
+          className='underline text-light-greenish text-xl 2xl:lg'
+        >
+          Head back home
+        </Link>
+      </div>
+    </div>
+  );
+}
