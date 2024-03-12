c1533b803bf6b414f8f46e196a7076d2890c9fc1
Zak Mayfield <zakmayfield@gmail.com>
2024-03-12 11:54:22 -0400

    fix: validator error message

--- src/features/cart/validator/validator.delivery-instructions.ts
+++ src/features/cart/validator/validator.delivery-instructions.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 
 export const DeliveryInstructionsValidator = z
   .object({
-    deliveryInstructions: z.string().email().min(3),
+    deliveryInstructions: z
+      .string()
+      .min(3, { message: 'Instructions must be 3 or more characters' }),
   })
   .required();
