c378c0b07a6820320890d7dbd85b716faf229629
Zak Mayfield <zakmayfield@gmail.com>
2024-03-22 15:11:45 -0400

    ui: style global error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -8,10 +8,22 @@ export default function Error({
   reset: () => void;
 }) {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <div>{error.digest}</div>
-      <button onClick={() => reset()}>Try again</button>
+    <div className='min-h-[35rem] flex items-center justify-center'>
+      <div className='py-12 px-3 flex flex-col gap-6 max-w-md w-full '>
+        <h1 className='text-2xl font-light'>Something went wrong!</h1>
+        <div className='bg-light-primary rounded-lg px-3 py-12'>
+          <p className='text-red-500'>
+            {error && typeof error === 'string' && error}
+          </p>
+        </div>
+
+        <button
+          onClick={() => reset()}
+          className='border p-2 rounded-lg w-36 mx-auto bg-light-greenish/70 text-white'
+        >
+          Try Again
+        </button>
+      </div>
     </div>
   );
 }
