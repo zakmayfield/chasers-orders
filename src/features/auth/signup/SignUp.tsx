14878d758c2ec04e828eddaad65d3b8eeea7df5b
Zak Mayfield <zakmayfield@gmail.com>
2024-03-22 09:35:36 -0400

    fix: step tracker import

--- src/features/auth/signup/SignUp.tsx
+++ src/features/auth/signup/SignUp.tsx
@@ -1,8 +1,7 @@
 'use client';
 import { useStepTracker } from './helpers.signup';
-import SignUpForm from './SignUpForm';
+import { SignUpForm, StepTracker } from './components';
 import FormSwitchLink from '../components/FormSwitchLink';
-import SignUpStepTracker from './SignUpStepTracker';
 
 export const SignUp = () => {
   const { step, setStep } = useStepTracker();
@@ -10,7 +9,7 @@ export const SignUp = () => {
   return (
     <div className='border-b col-span-6 col-start-4 xl:col-start-5 xl:col-span-4 py-6 px-12 font-extralight'>
       <h2 className='text-2xl mb-12'>Create an account</h2>
-      <SignUpStepTracker activeStep={step} />
+      <StepTracker activeStep={step} />
       <SignUpForm setStep={setStep} step={step} />
       <FormSwitchLink formType='sign-up' />
     </div>
