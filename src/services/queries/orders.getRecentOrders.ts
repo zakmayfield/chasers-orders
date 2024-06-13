7421d4c3b48a4ade4a2d2ad5d7a36f0a39c98485
Zak Mayfield <zakmayfield@gmail.com>
2024-06-13 14:33:55 -0400

    chore: utilize fetchHandler helper

--- src/services/queries/orders.getRecentOrders.ts
+++ src/services/queries/orders.getRecentOrders.ts
@@ -1,25 +1,10 @@
 import { OrderType } from '@/features/dashboard/recent-orders/RecentOrders';
+import { fetchHandler } from '@/shared/helpers';
 
-type GetOrdersType = {
-  (): Promise<OrderType[]>;
-};
-
-export const getRecentOrders: GetOrdersType = async () => {
-  try {
-    const fetchUrl = new URL(
-      `/api/orders/recent`,
-      process.env.NEXT_PUBLIC_BASE_URL
-    );
-    const response = await fetch(fetchUrl);
-
-    if (!response.ok) {
-      throw new Error(await response.text());
-    }
-
-    return response.json();
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    }
-  }
-};
+export const getRecentOrders = async (): Promise<OrderType[]> =>
+  await fetchHandler({
+    route: '/orders',
+    options: {
+      urlExtension: '/recent',
+    },
+  });
