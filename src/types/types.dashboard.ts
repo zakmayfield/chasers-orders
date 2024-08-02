b937531e78d8aa933f9fcb37025db801aac5c996
Zak Mayfield <zakmayfield@gmail.com>
2024-08-02 15:16:50 -0400

    refactor: consolidate user data and types

--- src/types/types.dashboard.ts
+++ src/types/types.dashboard.ts
@@ -8,25 +8,19 @@ import {
   Favorite,
 } from '@prisma/client';
 
-export type DashboardUserData = DashboardUser & {
-  contact: DashboardContact;
-  favorites: DashboardFavorite[];
-  orders: DashboardOrder[];
-  company: DashboardCompany;
+export type SecureUser = Omit<User, 'password'>;
+
+export type DashboardUserData = SecureUser & {
+  contact: Contact;
+  favorites: Favorite[];
+  orders: Order[];
+  company: Company & {
+    shippingAddress: ShippingAddress;
+    billingAddress: BillingAddress;
+  };
 };
 
 export type DashboardFetchState<T> = T | DashboardQueryError | null;
-
 export type DashboardQueryError = {
   error: string;
 };
-
-export type DashboardUser = Omit<User, 'password' | 'image'>;
-export type DashboardContact = Omit<Contact, 'userId'>;
-export type DashboardOrder = Omit<Order, 'userId'>;
-export type DashboardFavorite = Omit<Favorite, 'userId'>;
-export type DashboardCompany = CompanyType & {
-  shippingAddress: ShippingAddress;
-  billingAddress: BillingAddress;
-};
-type CompanyType = Omit<Company, 'userId'>;
