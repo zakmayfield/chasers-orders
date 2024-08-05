52b93e74c1855d2a80e1018355490a7782a653c1
Zak Mayfield <zakmayfield@gmail.com>
2024-08-05 14:52:25 -0400

    chore: cleanup unused type declaration

--- src/services/queries/getUserStatus.ts
+++ src/services/queries/getUserStatus.ts
@@ -1,10 +1,6 @@
 import { UserStatusAPIResponse } from '@/types/dashboard';
 import { fetchHandler } from '@/utils/fetch';
 
-export type UserStatus = {
-  (): Promise<UserStatusAPIResponse>;
-};
-
 export const getUserStatus = async (): Promise<UserStatusAPIResponse> =>
   await fetchHandler({
     route: '/user/status',
