ab44b8d8e78bd9fd18d73a0ef8e2fd846f6d1e57
Zak Mayfield <zakmayfield@gmail.com>
2024-08-07 16:17:47 -0400

    feat: implement custom query hook for shipping details

--- src/types/hooks.ts
+++ src/types/hooks.ts
@@ -3,6 +3,7 @@ import { QueryFunction, QueryKey } from '@tanstack/react-query';
 export enum QueryKeys {
   USER_STATUS = 'user-status',
   CART = 'cart',
+  SHIPPING = 'shipping',
 }
 
 export type UseCustomQueryParams<T> = {
