4054ea4afea50a4adc5bddb5279df0f79fa3ac95
Zak Mayfield <zakmayfield@gmail.com>
2024-08-09 14:56:47 -0400

    feat: implement get favorites hook for favorites service

--- src/shared/hooks/queries/index.ts
+++ src/shared/hooks/queries/index.ts
@@ -1,5 +1,6 @@
 export * from './useCustomQuery';
+export * from './useDashboardQuery';
 export * from './useGetCart';
+export * from './useGetFavorites';
 export * from './useGetRecentOrders';
 export * from './useGetUser';
-export * from './useDashboardQuery';
