37fa64efef3ea40d5ef7844a183df79d14416b15
Zak Mayfield <zakmayfield@gmail.com>
2024-08-11 18:07:19 -0400

    refactor: products feature directory

--- src/shared/hooks/queries/index.ts
+++ src/shared/hooks/queries/index.ts
@@ -1,5 +1,6 @@
 export * from './useCustomQuery';
 export * from './useDashboardQuery';
+export * from './useFavorites';
 export * from './useGetCart';
 export * from './useGetFavorites';
 export * from './useGetRecentOrders';
--- src/shared/hooks/queries/useFavorites.ts
+++ src/shared/hooks/queries/useFavorites.ts
@@ -1,27 +1,15 @@
 import { useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useCustomQuery } from './useCustomQuery';
+import { ExtendedFavorite } from '@/types/products';
+import { QueryKeys } from '@/types/hooks';
 import { getFavorites } from '@/services/queries/getFavorites';
-import type { ExtendedFavorite } from '@/types/products';
 
-interface UseFavorites {
-  ({ productId }: { productId?: string }): {
-    query: {
-      favorites: ExtendedFavorite[] | undefined;
-      isLoading: boolean;
-    };
-    favorite: {
-      isProductFavorited: boolean;
-      favoriteId: string | undefined;
-    };
-  };
-}
-// TODO: rework this hook
-export const useFavorites: UseFavorites = ({ productId }) => {
+export const useFavorites = ({ productId }: { productId?: string }) => {
   const [isProductFavorited, setIsProductFavorited] = useState(false);
   const [favoriteId, setFavoriteId] = useState<string | undefined>(undefined);
 
-  const { data: favorites, isLoading } = useQuery<ExtendedFavorite[], Error>({
-    queryKey: ['favorites'],
+  const { data: favorites, isLoading } = useCustomQuery<ExtendedFavorite[]>({
+    queryKey: [QueryKeys.FAVORITES],
     queryFn: getFavorites,
     staleTime: Infinity,
   });
