4e9ea685b3484efa85b880b040c6949a029dac31
Zak Mayfield <zakmayfield@gmail.com>
2024-08-11 18:39:55 -0400

    feat: create use is favorite hook

---
+++ src/shared/hooks/queries/useIsFavorite.ts
@@ -0,0 +1,28 @@
+import { ExtendedFavorite } from '@/types/products';
+import { useEffect, useState } from 'react';
+
+export const useIsFavorite = ({
+  favorites,
+  productId,
+}: {
+  favorites?: ExtendedFavorite[];
+  productId: string;
+}) => {
+  const [isProductFavorited, setIsProductFavorited] = useState(false);
+  const [favoriteId, setFavoriteId] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const isFavorite =
+      favorites && favorites.find((product) => product.juiceId === productId);
+
+    if (isFavorite) {
+      setFavoriteId(isFavorite.id);
+      setIsProductFavorited(!!isFavorite);
+    }
+  }, [favorites, productId]);
+
+  return {
+    favoriteId,
+    isProductFavorited,
+  };
+};
