6d01068138665ccbc8a2eebcd5f12e8d1ce4e9e0
Zak Mayfield <zakmayfield@gmail.com>
2024-08-14 15:57:36 -0400

    chore: add icon utils

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,6 +1,13 @@
 import { BsCartPlus } from 'react-icons/bs';
-import { PiHeart, PiHeartDuotone } from 'react-icons/pi';
+import {
+  PiHeart,
+  PiHeartDuotone,
+  PiStorefrontLight,
+  PiXCircleThin,
+} from 'react-icons/pi';
 
 export const CartIcon = BsCartPlus;
 export const HeartDuotoneIcon = PiHeartDuotone;
 export const HeartOutlineIcon = PiHeart;
+export const StoreIcon = PiStorefrontLight;
+export const XIcon = PiXCircleThin;
