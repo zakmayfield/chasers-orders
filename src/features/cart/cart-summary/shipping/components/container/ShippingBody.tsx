3806468ec2f667eed238322520819b9fb174bcd7
Zak Mayfield <zakmayfield@gmail.com>
2024-08-15 12:36:52 -0400

    chore: update shipping address component variables

--- src/features/cart/cart-summary/shipping/components/container/ShippingBody.tsx
+++ src/features/cart/cart-summary/shipping/components/container/ShippingBody.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import { DeliveryInstructions } from '@/features/cart/cart-summary/shipping/components/instructions';
-import { ShippingAddress } from '@prisma/client';
 import { ShippingData } from '@/types/user';
 
 interface ShippingBodyProps {
@@ -8,33 +7,29 @@ interface ShippingBodyProps {
 }
 
 export const ShippingBody: FC<ShippingBodyProps> = ({ data }) => {
-  const shippingAddress = data?.shippingAddress;
-  const companyName = data?.companyName;
   return (
     <div className='w-full p-4 font-extralight flex flex-col gap-3'>
-      <p className='text-lg font-light'>{companyName}</p>
-
-      <Address shippingAddress={shippingAddress} />
-
-      {/* Special Instructions */}
-      <DeliveryInstructions content={shippingAddress?.deliveryInstructions} />
+      <Address shippingData={data} />
+      <DeliveryInstructions
+        content={data?.shippingAddress?.deliveryInstructions}
+      />
     </div>
   );
 };
 
-function Address({
-  shippingAddress,
-}: {
-  shippingAddress: ShippingAddress | null | undefined;
-}) {
+function Address({ shippingData }: { shippingData: ShippingData | undefined }) {
   return (
-    <div className=' bg-light-primary p-3 rounded-lg flex flex-col gap-2'>
-      <p className=''>{shippingAddress?.streetAddress}</p>
-      <p>
-        <span>{shippingAddress?.city}</span>,{' '}
-        <span>{shippingAddress?.state}</span>
-      </p>
-      <p>{shippingAddress?.postalCode}, Canada</p>
+    <div>
+      <p className='text-lg font-light mb-3'>{shippingData?.companyName}</p>
+
+      <div className=' bg-light-primary p-3 rounded-lg flex flex-col gap-2'>
+        <p className=''>{shippingData?.shippingAddress?.streetAddress}</p>
+        <p>
+          <span>{shippingData?.shippingAddress?.city}</span>,{' '}
+          <span>{shippingData?.shippingAddress?.state}</span>
+        </p>
+        <p>{shippingData?.shippingAddress?.postalCode}, Canada</p>
+      </div>
     </div>
   );
 }
