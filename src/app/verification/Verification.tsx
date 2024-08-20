b2b84118d2a9a1bbec429a6f4fade45e9a62f950
Zak Mayfield <zakmayfield@gmail.com>
2024-08-20 14:27:45 -0400

    chore: verification heading

--- src/app/verification/Verification.tsx
+++ src/app/verification/Verification.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { FC } from 'react';
-import { Heading, VerificationResults } from './components';
+import { VerificationResults } from './components';
+import { Heading } from '@/shared/components/ui';
 
 interface VerificationProps {
   email: string | null | undefined;
@@ -11,8 +12,8 @@ interface VerificationProps {
 export const Verification: FC<VerificationProps> = ({ email, isVerified }) => {
   return (
     <div className='flex justify-center items-start rounded-lg'>
-      <div className='flex flex-col max-w-sm w-full'>
-        <Heading />
+      <div className='flex flex-col gap-3 max-w-sm w-full'>
+        <Heading as='h6' content='Verification' />
         <VerificationResults email={email} isVerified={isVerified} />
       </div>
     </div>
