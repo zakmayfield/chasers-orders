94640784c99ee3bd43d46873cb4fb926485ad927
Zak Mayfield <zakmayfield@gmail.com>
2024-08-20 13:54:32 -0400

    chore: ui page for component testing

---
+++ src/app/ui/components/Headings.tsx
@@ -0,0 +1,18 @@
+import { Heading } from '@/shared/components/ui';
+
+export const Headings = () => {
+  return (
+    <div className='flex flex-col gap-6'>
+      <h1>Headings</h1>
+
+      <div>
+        <Heading as='h1' content='Title: h1' />
+        <Heading as='h2' content='Title: h2' />
+        <Heading as='h3' content='Title: h3' />
+        <Heading as='h4' content='Title: h4' />
+        <Heading as='h5' content='Title: h5' />
+        <Heading as='h6' content='Title: h6' />
+      </div>
+    </div>
+  );
+};
---
+++ src/app/ui/page.tsx
@@ -0,0 +1,10 @@
+import { Headings } from './components';
+
+export default async function Page() {
+  return (
+    <div className='border w-3/4 mx-auto flex flex-col gap-12'>
+      {/* Headings */}
+      <Headings />
+    </div>
+  );
+}
