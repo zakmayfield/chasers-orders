1ccc0ad10525972cba0612c625f364e89958e704
Zak Mayfield <zakmayfield@gmail.com>
2024-08-21 16:25:14 -0400

    chore: flex center for container

--- src/shared/components/ui/Container.tsx
+++ src/shared/components/ui/Container.tsx
@@ -23,6 +23,7 @@ export type ContainerProps = {
   children?: React.ReactNode;
   className?: string;
   flex?: 'row' | 'col';
+  flexCenter?: boolean;
   padding?: 'sm' | 'md' | 'lg';
   margin?: 'sm' | 'md' | 'lg';
   rounded?: 'sm' | 'md' | 'lg';
@@ -36,6 +37,7 @@ export const Container = ({
   children,
   className,
   flex,
+  flexCenter,
   padding,
   margin,
   width,
@@ -49,13 +51,14 @@ export const Container = ({
         <Div
           className={`
             ${flex === 'row' ? 'flex items-start gap-3' : flex === 'col' ? 'flex flex-col gap-3' : 'block'} 
+            ${flexCenter && 'items-center justify-center'} 
             ${padding === 'sm' ? 'p-2' : padding === 'md' ? 'p-4' : padding === 'lg' ? 'p-6' : 'p-0'}
             ${margin === 'sm' ? 'm-2' : margin === 'md' ? 'm-4' : margin === 'lg' ? 'm-6' : 'm-0'}
             ${width === 'sm' ? 'max-w-sm w-full' : width === 'md' ? 'max-w-xl w-full' : width === 'lg' ? 'max-w-4xl w-full' : width === 'full' ? 'max-w-full w-full' : ''}
             ${rounded === 'sm' ? 'rounded-md' : rounded === 'md' ? 'rounded-xl' : rounded === 'lg' ? 'rounded-3xl' : 'rounded-none'}
             ${border && 'border'}
             ${className}
-          `}
+            `}
         >
           {children}
         </Div>
@@ -66,13 +69,14 @@ export const Container = ({
         <P
           className={`
             ${flex === 'row' ? 'flex items-center gap-3' : flex === 'col' ? 'flex flex-col gap-3' : 'block'} 
+            ${flexCenter && 'items-center justify-center'} 
             ${padding === 'sm' ? 'p-2' : padding === 'md' ? 'p-4' : padding === 'lg' ? 'p-6' : 'p-0'}
             ${margin === 'sm' ? 'm-2' : margin === 'md' ? 'm-4' : margin === 'lg' ? 'm-6' : 'm-0'}
             ${width === 'sm' ? 'max-w-sm w-full' : width === 'md' ? 'max-w-xl w-full' : width === 'lg' ? 'max-w-4xl w-full' : width === 'full' ? 'max-w-full w-full' : ''}
             ${rounded === 'sm' ? 'rounded-md' : rounded === 'md' ? 'rounded-xl' : rounded === 'lg' ? 'rounded-3xl' : 'rounded-none'}
             ${border && 'border'}
             ${className}
-          `}
+            `}
         >
           {children}
         </P>
@@ -83,13 +87,14 @@ export const Container = ({
         <Span
           className={`
             ${flex === 'row' ? 'flex items-center gap-3' : flex === 'col' ? 'flex flex-col gap-3' : 'inline'} 
+            ${flexCenter && 'items-center justify-center'} 
             ${padding === 'sm' ? 'p-2' : padding === 'md' ? 'p-4' : padding === 'lg' ? 'p-6' : 'p-0'}
             ${margin === 'sm' ? 'm-2' : margin === 'md' ? 'm-4' : margin === 'lg' ? 'm-6' : 'm-0'}
             ${width === 'sm' ? 'max-w-sm w-full' : width === 'md' ? 'max-w-xl w-full' : width === 'lg' ? 'max-w-4xl w-full' : width === 'full' ? 'max-w-full w-full' : ''}
             ${rounded === 'sm' ? 'rounded-md' : rounded === 'md' ? 'rounded-xl' : rounded === 'lg' ? 'rounded-3xl' : 'rounded-none'}
             ${border && 'border'}
             ${className}
-          `}
+            `}
         >
           {children}
         </Span>
@@ -100,14 +105,15 @@ export const Container = ({
         <Loader
           className={`
             animate-pulse 
-              ${flex === 'row' ? 'flex items-center gap-3' : flex === 'col' ? 'flex flex-col gap-3' : 'block'} 
-              ${padding === 'sm' ? 'p-2' : padding === 'md' ? 'p-4' : padding === 'lg' ? 'p-6' : 'p-0'}
-              ${margin === 'sm' ? 'm-2' : margin === 'md' ? 'm-4' : margin === 'lg' ? 'm-6' : 'm-0'}
-              ${width === 'sm' ? 'max-w-sm w-full' : width === 'md' ? 'max-w-xl w-full' : width === 'lg' ? 'max-w-4xl w-full' : width === 'full' ? 'max-w-full w-full' : ''}
-              ${animateColor === 'dark' ? 'bg-slate-100' : animateColor === 'darker' ? 'bg-slate-200' : animateColor === 'darkest' ? 'bg-slate-300' : 'bg-slate-100'}
-              ${rounded === 'sm' ? 'rounded-md' : rounded === 'md' ? 'rounded-xl' : rounded === 'lg' ? 'rounded-3xl' : 'rounded-md'}
-              ${border && 'border'}
-              ${className}
+            ${flex === 'row' ? 'flex items-center gap-3' : flex === 'col' ? 'flex flex-col gap-3' : 'block'} 
+            ${flexCenter && 'items-center justify-center'} 
+            ${padding === 'sm' ? 'p-2' : padding === 'md' ? 'p-4' : padding === 'lg' ? 'p-6' : 'p-0'}
+            ${margin === 'sm' ? 'm-2' : margin === 'md' ? 'm-4' : margin === 'lg' ? 'm-6' : 'm-0'}
+            ${width === 'sm' ? 'max-w-sm w-full' : width === 'md' ? 'max-w-xl w-full' : width === 'lg' ? 'max-w-4xl w-full' : width === 'full' ? 'max-w-full w-full' : ''}
+            ${animateColor === 'dark' ? 'bg-slate-100' : animateColor === 'darker' ? 'bg-slate-200' : animateColor === 'darkest' ? 'bg-slate-300' : 'bg-slate-100'}
+            ${rounded === 'sm' ? 'rounded-md' : rounded === 'md' ? 'rounded-xl' : rounded === 'lg' ? 'rounded-3xl' : 'rounded-md'}
+            ${border && 'border'}
+            ${className}
             `}
         >
           {children}
