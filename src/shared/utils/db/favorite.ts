f11ef818360181be67e611e19142f13171890087
Zak Mayfield <zakmayfield@gmail.com>
2024-08-27 17:04:42 -0400

    chore: favorite db utils methods

--- src/shared/utils/db/favorite.ts
+++ src/shared/utils/db/favorite.ts
@@ -2,10 +2,66 @@
 import { db } from '@/lib/prisma';
 import { TFavorite, TFavoriteWithProduct } from '@/shared/types/Favorite';
 
+//^ POST
+type TAddToFavorites = (props: {
+  user_id: string;
+  product_id: string;
+}) => Promise<TFavorite>;
+export const addToFavorites: TAddToFavorites = async ({
+  user_id,
+  product_id,
+}) => {
+  const favorite = await db.favorite.create({
+    data: {
+      user_id,
+      product_id,
+    },
+  });
+  return favorite;
+};
+
+//^ DELETE
+type TDeleteFromFavorites = (props: {
+  favorite_id: string;
+}) => Promise<TFavorite>;
+export const deleteFromFavorites: TDeleteFromFavorites = async ({
+  favorite_id,
+}) => {
+  const favorite = await db.favorite.delete({
+    where: { favorite_id },
+  });
+  return favorite;
+};
+
+//^ MISC
+type TToggleFavorite = (props: {
+  isFavorited: boolean;
+  user_id?: string;
+  product_id?: string;
+  favorite_id?: string;
+}) => Promise<TFavorite>;
+export const toggleFavorite: TToggleFavorite = async ({
+  isFavorited,
+  user_id,
+  product_id,
+  favorite_id,
+}) => {
+  if (isFavorited) {
+    const del = await deleteFromFavorites({ favorite_id: favorite_id! });
+    return del;
+  } else {
+    const add = await addToFavorites({
+      user_id: user_id!,
+      product_id: product_id!,
+    });
+    return add;
+  }
+};
+
+//^ GET
 type TGetFavoritesByUserId = (props: {
   user_id: string;
 }) => Promise<TFavorite[]>;
-
 export const getFavoritesByUserId: TGetFavoritesByUserId = async ({
   user_id,
 }) => {
