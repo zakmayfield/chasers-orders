1121dcdfe67404c145a61ba223952ff09afc01c5
Zak Mayfield <zakmayfield@gmail.com>
2024-08-28 15:43:31 -0400

    chore: cleanup cart hooks

--- src/shared/hooks/data/cart/useCart.ts
+++ src/shared/hooks/data/cart/useCart.ts
@@ -1,39 +1,33 @@
 import { QueryKeys } from '@/shared/types/Cache';
 import { useCustomQuery } from '../../custom';
-import {
-  getCartItem,
-  getCartItems,
-  getCartWithItemsAndProductVariants,
-} from '@/shared/utils/db/cart';
+import { cartServices } from '@/shared/utils/services/cartServices';
 
-export const useGetCart = ({ user_id }: { user_id: string }) => {
+export const useGetCart = () => {
   const { data, isLoading, error } = useCustomQuery({
     queryKey: [QueryKeys.CART],
-    queryFn: async () => await getCartWithItemsAndProductVariants({ user_id }),
+    queryFn: async () => await cartServices.getCartWithItemsAndVariants(),
     staleTime: Infinity,
   });
   return { data, isLoading, error };
 };
 
-export const useGetCartItems = ({ cart_id }: { cart_id: string }) => {
+export const useGetCartItems = () => {
   const { data, isLoading, error } = useCustomQuery({
     queryKey: [QueryKeys.CART_ITEMS],
-    queryFn: async () => await getCartItems({ cart_id }),
+    queryFn: async () => await cartServices.getCartItems(),
     staleTime: Infinity,
   });
   return { data, isLoading, error };
 };
 
 export const useGetCartItem = ({
-  cart_id,
   product_variant_id,
 }: {
-  cart_id: string;
   product_variant_id: string;
 }) => {
   const { data, isLoading, error } = useCustomQuery({
     queryKey: [QueryKeys.CART_ITEM, product_variant_id],
-    queryFn: async () => await getCartItem({ cart_id, product_variant_id }),
+    queryFn: async () => await cartServices.getCartItem({ product_variant_id }),
     staleTime: Infinity,
   });
   return { data, isLoading, error };
