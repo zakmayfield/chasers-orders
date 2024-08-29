28d7dfe862812b4f588620e568904ecd89a2a148
Zak Mayfield <zakmayfield@gmail.com>
2024-08-29 12:19:46 -0400

    chore: cleanup cart endpoints

--- src/app/api/cart/items/:product_variant_id/route.ts
+++ src/app/api/cart/items/:product_variant_id/route.ts
@@ -1,4 +1,3 @@
-import { db } from '@/lib/prisma';
 import { checkAuthentication } from '@/shared/utils/api/checkAuthentication';
 import { errorResponse } from '@/shared/utils/api/errorResponse';
 import { getSearchParams } from '@/shared/utils/api/getSearchParams';
@@ -7,15 +6,11 @@ import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
   try {
-    const { user_id } = await checkAuthentication();
+    const { cart_id } = await checkAuthentication();
     const product_variant_id = getSearchParams(
       req.nextUrl.searchParams,
       'product_variant_id'
     );
-    const { cart_id } = await db.cart.findUniqueOrThrow({
-      where: { user_id },
-      select: { cart_id: true },
-    });
     const data = await getCartItem({ cart_id, product_variant_id });
     return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
--- src/app/api/cart/items/route.ts
+++ src/app/api/cart/items/route.ts
@@ -1,15 +1,10 @@
-import { db } from '@/lib/prisma';
 import { checkAuthentication } from '@/shared/utils/api/checkAuthentication';
 import { errorResponse } from '@/shared/utils/api/errorResponse';
 import { getCartItems } from '@/shared/utils/db/cart';
 
 export async function GET() {
   try {
-    const { user_id } = await checkAuthentication();
-    const { cart_id } = await db.cart.findUniqueOrThrow({
-      where: { user_id },
-      select: { cart_id: true },
-    });
+    const { cart_id } = await checkAuthentication();
     const data = await getCartItems({ cart_id });
     return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
