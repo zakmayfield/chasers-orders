50627d34d27d252fec0a06622563f45885f2cb7e
Zak Mayfield <zakmayfield@gmail.com>
2024-09-03 16:41:30 -0400

    cleanup

--- src/features/dashboard/organisms/PasswordChange.tsx
+++ src/features/dashboard/organisms/PasswordChange.tsx
@@ -3,10 +3,10 @@ import { ChangePasswordForm } from '../molecules/ChangePasswordForm';
 
 export const PasswordChange = () => {
   return (
-    <ContentTemplate title='Change Password' headingAs='h4'>
+    <ContentTemplate title='Password' headingAs='h4'>
       <Container as='div' flex='col' paddingX='lg'>
         <Container as='div' flex='col'>
-          <Heading as='h5' content='Password' />
+          <Heading as='h5' content='Change Password' />
           <ChangePasswordForm />
         </Container>
       </Container>
