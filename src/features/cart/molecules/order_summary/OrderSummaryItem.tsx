3b1b23ff5fc1293196bc5335607f568ea5902c84
Zak Mayfield <zakmayfield@gmail.com>
2024-09-10 14:51:46 -0400

    cleanup

--- src/features/cart/molecules/order_summary/OrderSummaryItem.tsx
+++ src/features/cart/molecules/order_summary/OrderSummaryItem.tsx
@@ -5,19 +5,15 @@ export const OrderSummaryItem = ({ cartItem }: { cartItem: TCartItem }) => {
   const { quantity, product_variant } = cartItem;
 
   return (
-    <ContentWrapper
-      padding='sm'
-      flex='row'
-      className='border items-start justify-between'
-    >
-      <ContentWrapper flex='col' className='items-start'>
+    <ContentWrapper padding='sm' flex='row' className='border items-start'>
+      <ContentWrapper flex='col' width='md' className='items-start'>
         <Text>{product_variant?.product?.name}</Text>
         <Text className=''>
           {product_variant?.product?.category?.name.toLowerCase()}
         </Text>
       </ContentWrapper>
 
-      <ContentWrapper flex='col' className='items-end'>
+      <ContentWrapper flex='col' className='items-end sm:items-start'>
         <Text>x{quantity}</Text>
         <Text className=''>{product_variant?.size}</Text>
       </ContentWrapper>
