aa1a428854c6b9706b16e8a4b15f8ae9df1648b0
Zak Mayfield <zakmayfield@gmail.com>
2024-09-11 11:04:47 -0400

    place order feature

--- src/features/cart/atoms/place_order/PlaceOrderButton.tsx
+++ src/features/cart/atoms/place_order/PlaceOrderButton.tsx
@@ -1,7 +1,32 @@
 import { Btn } from '@/shared/components/ui';
+import { useGetCart } from '@/shared/hooks/data/cart/useCart';
+import { useCreateOrder } from '@/shared/hooks/data/orders/useOrders';
 
 type TPlaceOrderButtonProps = {};
 
 export const PlaceOrderButton = (props: TPlaceOrderButtonProps) => {
-  return <Btn text='Place Order' bgColor='green' width='full' />;
+  const { cart, isLoading: loadingCart } = useGetCart();
+  const { mutate, isLoading: loadingCreateOrder } = useCreateOrder();
+
+  const loading = loadingCart || loadingCreateOrder;
+
+  const line_items = cart?.items.map((item) => {
+    return {
+      product_variant_id: item.product_variant_id,
+      quantity: item.quantity,
+    };
+  });
+
+  const order = () => mutate({ line_items: line_items! });
+
+  return (
+    <Btn
+      text='Place Order'
+      bgColor='green'
+      width='full'
+      isDisabled={loading}
+      isLoading={loadingCreateOrder}
+      handleClick={order}
+    />
+  );
 };
--- src/features/cart/organisms/PlaceOrder.tsx
+++ src/features/cart/organisms/PlaceOrder.tsx
@@ -1,28 +1,10 @@
 import { ContentWrapper } from '@/shared/components/containers';
-import { useGetCart } from '@/shared/hooks/data/cart/useCart';
-import { useCreateOrder } from '@/shared/hooks/data/orders/useOrders';
 import { PlaceOrderButton } from '@/features/cart/atoms/place_order/PlaceOrderButton';
 
 export const PlaceOrder = () => {
-  const { cart } = useGetCart();
-  const { mutate: createOrder } = useCreateOrder();
-
   return (
     <ContentWrapper>
       <PlaceOrderButton />
     </ContentWrapper>
   );
-  // <Container as='div'>
-  //   <Btn
-  //     text='Place Order'
-  //     bgColor='green'
-  //     width='full'
-  //     height='lg'
-  //     isDisabled={
-  //       cart.isLoading || cart.data?.items.length === 0 || createOrderLoading
-  //     }
-  //     isLoading={createOrderLoading}
-  //     handleClick={order}
-  //   />
-  // </Container>
 };
